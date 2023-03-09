ab2a98d0776a779fb196e44744d271d986f3e49c
justin.yee2 <justin.yee2@linux13-wd.cs.ucalgary.ca>
2023-03-09 13:59:15 -0700

    Added a testclass that that we can add test cases into. It can be deleted later however, it's just a template.

---
+++ com.autovend.software.test/src/TempTest.java
@@ -0,0 +1,33 @@
+//Justin Yee (30113485),
+//Huayong Gao (),
+//Mohammad Khan (),
+//Quinn Leonard (),
+//Sohaib Shahid (),
+//Jaxon Sloan (),
+
+package src;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TempTest {
+
+	
+	@Before
+	public void generalSetup() {
+		
+	}
+
+}
+
+	
+
